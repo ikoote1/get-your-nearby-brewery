31d78af85576e32eb1bed26f8cae729751e66d4d
ikoote1 <ikootepreim@gmail.com>
2023-04-17 16:52:17 +0300

    Seting up the progect

--- src/redux/category/categorySlice.js
+++ src/redux/category/categorySlice.js
@@ -3,7 +3,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const getData = createAsyncThunk('brewery/getData', async () => {
     try {
-        const response = await fetch(url);
+        const response = await fetch("https://api.openbrewerydb.org/v1/breweries");
         const data = response.json();
         return data;
     } catch (error) {
@@ -17,8 +17,27 @@ const categorySlice = createSlice(
         initialState: {
             dataStore: [],
             isLoading: true,
-        }
-    }
+        },
+        extraReducers: (builder) => {
+            builder
+              .addCase(getData.pending, (state) => ({
+                ...state,
+                isLoading: true,
+              }))
+              .addCase(getData.fulfilled, (state, action) => (
+                console.log(getData),
+                {
+                  ...state,
+                  isLoading: false,
+                  dataStore: action.payload,
+                }))
+              .addCase(getData.rejected, (state) => ({
+                ...state,
+                isLoading: false,
+                error: 'state error',
+              }));
+          },
+    },
 );
 
 export default categorySlice.reducer
