6461fdc9191a7fdab4dfb4ad55c0dd4a81f12acb
ikoote1 <ikootepreim@gmail.com>
2023-04-17 16:28:24 +0300

    Seting up the progect

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        
+        <Route path='/' element={<category />} />
       </Routes>
       <h1>ikoote</h1>
     </div>
