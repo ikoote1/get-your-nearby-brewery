04f8827d1a06c720503e20d033c30e0a3f9219dd
ikoote1 <ikootepreim@gmail.com>
2023-04-17 16:25:22 +0300

    Seting up the progect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        
+      </Routes>
       <h1>ikoote</h1>
     </div>
   );
